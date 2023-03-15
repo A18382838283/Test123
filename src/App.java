a6a8bf8f7b63969a69fe025e7bda55dfd7a1b6ea
Sergej.stk <sergejsteinsiek@gmail.com>
2023-03-15 17:37:27 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App
+ {
+
+    public static void writemes()
+    {
+        System.out.println("Ich heiße Sergej Steinsiek aus Simbach am Inn");
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello, World!");
+        writemes();
+    }
+}
